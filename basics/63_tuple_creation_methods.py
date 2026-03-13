a6a9038e76fe29e4392f69578bff7e5d64664f13
Bhavishay <Bhavi.bish@gmail.com>
2026-03-14 00:09:34 +0530

    Tuple

---
+++ basics/63_tuple_creation_methods.py
@@ -0,0 +1,15 @@
+# Creating an empty tuple
+tup = ()
+print(tup)
+
+# Creating a tuple using strings
+tup = ('Python', 'For')
+print(tup)
+
+# Converting a list into a tuple
+li = [1, 2, 4, 5, 6]
+print(tuple(li))
+
+# Creating a tuple using the tuple() constructor with a string
+tup = tuple('Beginner')
+print(tup)
---
+++ basics/64_tuple_examples.py
@@ -0,0 +1,13 @@
+# Creating a tuple with different data types
+tup = (5, 'Welcome', 7, 'Geeks')
+print(tup)
+
+# Creating a tuple with nested tuples
+tup1 = (0, 1, 2, 3)
+tup2 = ('python', 'geek')
+tup3 = (tup1, tup2)
+print(tup3)
+
+# Creating a tuple with repetition
+tup1 = ('Geeks',) * 3
+print(tup1)
